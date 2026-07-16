accbaec72446e23bfeb8f6ab7b3e6c733782c4a2
Aryan Varmora <aryanvarmora8@gmail.com>
2026-07-16 16:30:08 -0400

    Simple working ToDoList.rb added

---
+++ ToDoList.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+# Create an empty array
+
+# Start the menu loop
+
+# Display menu
+
+# Read the user's choice
+
+# If choice is 1:
+# Ask for a task
+# Add task to array
+
+# If choice is 2:
+# Check whether array is empty
+# Otherwise display every task
+
+# If choice is 3:
+# Display goodbye
+# Stop the loop
+
+# Otherwise:
+# Display invalid choice
+
+tasks = []
+
+loop do
+  puts "Select an option:"
+  puts "1. Add a task"
+  puts "2. View tasks"
+  puts "3. Exit"
+
+  menu = gets.chomp.to_i
+
+  case menu
+  when 1
+    puts "Please type your daily task:"
+    input = gets.chomp
+    tasks << input
+    puts "Task added successfully."
+
+  when 2
+    if tasks.empty?
+      puts "You have no tasks."
+    else
+      tasks.each_with_index do |task, index|
+        puts "#{index + 1}. #{task}"
+      end
+    end
+
+  when 3
+    puts "Goodbye!"
+    break
+
+  else
+    puts "Invalid option. Please choose 1, 2, or 3."
+  end
+end
