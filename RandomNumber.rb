7ec769bc6f7fecbb2c41a1c43c6c2dfc407b1111
Aryan Varmora <aryanvarmora8@gmail.com>
2026-07-16 13:22:55 -0400

    RandomNumber.rb add, with string handling

---
+++ RandomNumber.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require "colorize"
+
+random_number = rand(1..100)
+count = 0
+guessed_correctly = false
+
+while count < 100
+  print "Enter your guess between 1 and 100: "
+
+  input = gets.chomp
+
+  begin
+    guess = Integer(input)
+  rescue ArgumentError
+    puts "Please enter a valid whole number.".red
+
+    # Restart the while loop here
+    next
+  end
+
+  unless guess.between?(1, 100)
+    puts "Your number must be between 1 and 100.".red
+
+    # Restart the while loop here
+    next
+  end
+
+  # Increase the count only after the input is valid
+  count +=1
+
+  if guess == random_number
+    guessed_correctly = true
+
+    puts "Correct! You found the number in #{count} guesses."
+
+    # Stop the while loop
+    break
+  elsif guess < random_number
+    puts "Your guess was too low."
+  else
+    puts "Your guess was too high."
+  end
+end
+
+# Show this only when the user did not win
+unless guessed_correctly
+  puts "Sorry, you ran out of guesses.".red
+  puts "The number was #{random_number}."
+end
